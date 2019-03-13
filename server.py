0afce51a6ef2f15e61fa369610520aa7f75e3273
LisaN90 <47413366+LisaN90@users.noreply.github.com>
2019-03-13 21:52:10 +0100

    Doch wieder ohne Docker.

---
+++ server.py
@@ -0,0 +1,64 @@
+import os
+
+from flask import Flask
+
+from fastai import *
+from fastai.vision import *
+from fastai import basic_train
+
+export_file_name = 'Fertigstellungsgrad.pkl'
+
+classes = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+path = Path(__file__).parent
+
+# load the learner
+try:
+    learn = load_learner(path, export_file_name)
+    return learn
+except RuntimeError as e:
+    if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+        print(e)
+        message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+        raise RuntimeError(message)
+    else:
+        raise
+
+"""Create and configure an instance of the Flask application."""
+app = Flask(__name__, instance_relative_config=True)
+
+app.config.from_mapping(
+    # a default secret that should be overridden by instance config
+    SECRET_KEY='dev',
+    # store the database in the instance folder
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
+
+if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    # load the test config if passed in
+    app.config.update(test_config)
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+    
+# register the database commands
+from flaskr import db
+db.init_app(app)
+
+# apply the blueprints to the app
+from flaskr import auth, wbs
+app.register_blueprint(auth.bp)
+app.register_blueprint(wbs.bp)
+
+# make url_for('index') == url_for('blog.index')
+# in another app, you might define a separate main index here with
+# app.route, while giving the blog blueprint a url_prefix, but for
+# the tutorial the blog will be the main index
+app.add_url_rule('/', endpoint='index')
+    
+
