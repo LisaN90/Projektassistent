f91edce1f80d14670389f77d0a5f8b77510f6022
LisaN90 <47413366+LisaN90@users.noreply.github.com>
2019-03-18 18:44:59 +0100

    Final Version 1.0

--- app/static/client.js
+++ app/static/client.js
@@ -1,37 +1,57 @@
 const el = x => document.getElementById(x);
 
+document.addEventListener('DOMContentLoaded', function() {
+    el('analyze-button').addEventListener('press', function(oEvent) {
+        analyze();
+    });
+});
+
+function clearMessageBox() {
+    el('messagebox').innerHTML = '';
+}
+
+function addToMessageBox(messageType, messageText) {
+    const messageStripText = `<ui5-messagestrip type="${messageType}" style="margin-bottom: 0.5rem" hide-close-button>${messageText}</ui5-messagestrip>`;
+    el('messagebox').innerHTML = el('messagebox').innerHTML + messageStripText;
+}
+
 function analyze() {
     const inputTitle = el('title').value;
     const inputText = el('statustext').value;
+    clearMessageBox();
     if (inputTitle.length === 0 || inputText.length === 0) {
-        alert('Titel und Status müssen gefüllt sein!');
+        addToMessageBox('Negative', 'Titel und Status müssen gefüllt sein!');
         return;
     }
     el('analyze-button').innerHTML = 'Analysiere...';
     const xhr = new XMLHttpRequest();
     const loc = window.location;
-	const url = encodeURI(`${loc.protocol}//${loc.hostname}/analyze?title=${inputTitle}&text=${inputText}`);
+    var url = '';
+    if (loc.hostname === '') { // used for local development
+        url = encodeURI(`https://projektassistent-af.onrender.com/analyze?title=${inputTitle}&text=${inputText}`);
+    } else {
+        url = encodeURI(`${loc.protocol}//${loc.hostname}/analyze?title=${inputTitle}&text=${inputText}`);
+    }
     xhr.open('GET', url, true);
     xhr.onerror = function() {alert (xhr.responseText);}
     xhr.onload = function(e) {
-		console.log('--this');
-		console.log(this);
+        console.log('--this');
+        console.log(this);
         if (this.readyState === 4 && this.status === 200) {
             console.log('--responseText: '); console.log(e.target.responseText);
             const response = JSON.parse(e.target.responseText);
-            el('status').innerHTML = response.computed_status;
-			switch (response.computed_trafficlight) {
-				case 'Grün':
-					el('traffic-light').src = '../static/Smiley-Green.jpg';
-					break;
-				case 'Gelb':
-					el('traffic-light').src = '../static/Smiley-Yellow.jpg';
-					break;
-				case 'Rot':
-					el('traffic-light').src = '../static/Smiley-Red.jpg';
-					break;
-			}
-			el('results').style.visibility = 'visible';
+            switch (response.computed_trafficlight) {
+                case 'Grün':
+                    addToMessageBox('Positive', 'Projekt läuft positiv');
+                    break;
+                case 'Gelb':
+                    addToMessageBox('Warning', 'Projekt läuft mit Warungen');
+                    break;
+                case 'Rot':
+                    addToMessageBox('Negative', 'Projekt läuft schlecht');
+                    break;
+            }
+            addToMessageBox('Information', `Berechneter Projektfortschritt: ${response.computed_status}`);
         }
         el('analyze-button').innerHTML = 'Analysieren';
     }
